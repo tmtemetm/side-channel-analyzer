93f8a76609effd820b766e7099e47503c0adff05
Teemu Mäki <teemu.maki@znapio.com>
2021-01-13 14:42:04 +0200

    Create Project From Spring Initializr

---
+++ src/main/java/fi/tmtemetm/sidechannelanalyzer/SideChannelAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package fi.tmtemetm.sidechannelanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideChannelAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideChannelAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/tmtemetm/sidechannelanalyzer/SideChannelAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tmtemetm.sidechannelanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideChannelAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
